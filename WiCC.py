1fa4bbc3ffd3adc8d4873659a84d2edb62596e3a
MiguelYanes <miguelyanes@hotmail.es>
2019-04-03 13:11:34 +0100

    Polished verbose levels. Implemented a non-verbose option, and a basic verbose with -v. Also when selecting the help, the program now shows the help and then closes itself.

--- WiCC.py
+++ WiCC.py
@@ -5,6 +5,12 @@ import time
 import threading
 
 
+verbose_level = 0
+
+def show_message(message):
+    if verbose_level >= 1:
+        print(message)
+
 if __name__ == '__main__':
     # check root privilege
     if os.getuid() != 0:
@@ -43,22 +49,34 @@ if __name__ == '__main__':
     for arg in args:
         if arg == '-h':
             headless = True
-            print("*** Running program headless\n")
+            print(" *** Running program headless\n")
         elif arg == '-a':
             auto_select = True
-            print("*** Auto-select network interface\n")
+            print(" *** Auto-select network interface\n")
         elif arg == '--help':
-            print("*** Use option -h to run the program headless")
-            print("*** Use option -a to auto-select the first available network interface\n")
-        elif arg == '-vv':
-            control.set_verbose_level(2)
-            print("*** Verbose level set to 2\n")
-        elif arg == '-vvv':
-            control.set_verbose_level(3)
-            print("*** Verbose level set to 3\n")
+            print(" ***  -h to run the program headless")
+            print(" ***  -a to auto-select the first available network interface")
+            print(" ***  -v to select the verbose level for the program")
+            print("\t-v   for level 1 (basic output)")
+            print("\t-vv  for level 2 (advanced output)")
+            print("\t-vvv for level 3 (advanced output and executed commands)\n")
+            sys.exit(0)
+        elif '-v' in arg:
+            if arg == '-v':
+                control.set_verbose_level(1)
+                verbose_level = 1
+                print("*** Verbose level set to " + str(verbose_level) + "\n")
+            elif arg == '-vv':
+                control.set_verbose_level(2)
+                verbose_level = 2
+                print("*** Verbose level set to " + str(verbose_level) + "\n")
+            elif arg == '-vvv':
+                control.set_verbose_level(3)
+                verbose_level = 3
+                print("*** Verbose level set to " + str(verbose_level) + "\n")
         else:
             print("*** Unrecognized option " + arg)
-            print("*** Use option --help to view the help. Only for debugging purposes\n")
+            print("*** Use option --help to view the help and finish execution. Only for debugging purposes\n")
 
     if headless:
         view_thread = threading.Thread(target=control.start_view, args=(True,))
@@ -68,49 +86,49 @@ if __name__ == '__main__':
     view_thread.join(1)
     while not exit:
         if control.has_selected_interface():
-            print("Selected interface: " + control.selectedInterface)
+            show_message("Selected interface: " + control.selectedInterface)
             control.scan_networks()
-            print("Start scanning available networks...")
+            show_message("Start scanning available networks...")
             time.sleep(3)
             while not control.selectedNetwork and control.running_scan():
                 time.sleep(1)
-                print("\t... Scanning networks ...")
+                show_message("\t... Scanning networks ...")
                 if not control.filter_networks():
                     time.sleep(1)
                     control.stop_scan()
                     time.sleep(1)
-                    print(" * An error ocurred, please, re-select the interface")
+                    show_message(" * An error ocurred, please, re-select the interface")
                     control.selectedInterface = ""
                     control.last_selectedInterface = ""
                     control.model.interfaces = []
                     while not control.has_selected_interface():
                         control.scan_interfaces(auto_select)
-                        print("Scanning interfaces")
+                        show_message("Scanning interfaces")
                         time.sleep(1)
-                    print("Selected interface: " + control.selectedInterface)
+                    show_message("Selected interface: " + control.selectedInterface)
                     control.scan_networks()
-                    print("Start scanning available networks...")
+                    show_message("Start scanning available networks...")
                     time.sleep(3)
-            print("\n * Network scanning stopped * \n")
+            show_message("\n * Network scanning stopped * \n")
             while not control.selectedNetwork:
                 # waits until a network is selected
                 time.sleep(1)
-            print("Selected network: " + str(control.selectedNetwork))
-            print("\nStarting attack...\n")
+            show_message("Selected network: " + str(control.selectedNetwork))
+            show_message("\nStarting attack...\n")
 
             while not control.cracking_completed and not control.is_cracking_network():
-                print("\t... Cracking network ...")
+                show_message("\t... Cracking network ...")
                 time.sleep(1)
 
             while control.is_cracking_network():
-                print("\t... Cracking password ...")
+                show_message("\t... Cracking password ...")
                 # print(control.check_cracking_status())
                 time.sleep(1)
 
-            print("Cracking process finished.")
+            show_message("Cracking process finished.")
             sys.exit(0)
         else:
-            print("Scanning interfaces")
+            show_message("Scanning interfaces")
             control.scan_interfaces(auto_select)
             time.sleep(1)
             if control.get_interfaces() == "":
