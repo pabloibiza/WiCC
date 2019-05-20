85594f14600029485c4a0ff86b74089e4df8fca8
MiguelYanes <miguelyanes@hotmail.es>
2019-05-20 10:47:28 +0100

    Fixed typo in header

--- wicc_wep.py
+++ wicc_wep.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-    WWiCC (Wifi Cracking Camp)
+    WiCC (Wifi Cracking Camp)
     GUI tool for wireless cracking on WEP and WPA/WPA2 networks.
     Project developed by Pablo Sanz Alguacil and Miguel Yanes Fernández, as the Group Project for the 3rd year of the
     Bachelor of Sicence in Computing in Digital Forensics and CyberSecurity, at TU Dublin - Blanchardstown Campus
